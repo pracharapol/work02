d64b7d3833e2444cb6d21ab0ea42a56353dd0ed1
PRACHARAPOL JARUVANAWAT <63010630@KMITL.AC.TH>
2020-08-14 20:16:52 +0700

    1

---
+++ work02/Source.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main() {
+	int x = 3;
+	printf("%d\n", x);
+	printf(" hello world \n");
+	int y;
+	scanf_s("%d", &y);
+	if (y == 20) {
+		printf("my name is kong");
+
+
+	}
+	else {
+		printf("error");
+	}
+	return 0;
+
+}
